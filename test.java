2b640815683f58719de56dea8e5790c386160b53
YaodongJava <yd8500721@163.com>
2017-06-05 21:24:38 +0800

    add static method
    add alwaySayHello method and main method.

--- test.java
+++ test.java
@@ -3,5 +3,15 @@ public class test{
 
 	public String sayHello(){
 	return "hello";
-}
+	}
+
+	public static String alwaySayHello(){
+
+		return "hello!";
+
+	}
+	public static void main(String args[]){
+
+		test
+	}
 }
